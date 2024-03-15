db8602972c7d8679b266de70523ad7da252615ec
Prashant Jinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-03-16 00:31:14 +0530

    Improve Fetching #2

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -77,7 +77,7 @@ function Main() {
 
             <div className='main-bottom  bottom-0 w-[99%] max-w-[900px] m-auto pt-0 pr-3 lg:pr-5 fixed '>
                     <div className='search-box flex items-center gap-5 justify-between  bg-[#f0f7f9] rounded-[50px] py-3  px-5    '>
-                        <input onChange={(e)=>setinput(e.target.value)}  value={input} className='lg:w-[75%] w-full lg:py-2 lg:px-2 mx-3  py-5 outline-none bottom-0 bg-transparent text-[18px]' type='text' placeholder="Enter a prompt here" />
+                        <input onChange={(e)=>setinput(e.target.value)}  value={input} className='lg:w-[75%] w-full lg:py-2 lg:px-2 mx-3  py-5 outline-none bottom-0 bg-transparent text-[18px]' type='text' placeholder="Enter a prompt " />
                         <div className="flex items-center lg:gap-4 gap-6 pr-3 ">
                             <img src={assets.gallery_icon} alt="#" />
                             <img src={assets.mic_icon} alt="#" />
