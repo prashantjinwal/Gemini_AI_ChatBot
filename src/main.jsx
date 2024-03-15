adcda1bdfb58c23c382e1ac9a7c4577584533d8c
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-03-15 16:51:30 +0530

    Integrating API

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ContextProvider from './context/Context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>,
+  </ContextProvider>,
 )
