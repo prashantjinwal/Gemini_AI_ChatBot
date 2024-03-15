af6dd4af05c1fcc959a21135839da74dd5ed7d03
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-03-15 23:01:56 +0530

    Responsive

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,47 +9,49 @@ function Main() {
 
   return (
     <>
-    <div className='main flex-1 relative min-h-[100vh] pb-[1em] px-4'>
+    <div className='main flex-1 relative min-h-[100vh] pb-[1em] px-2 lg:px-5'>
         <div className='nav flex items-center  justify-between text-xl p-5'>
-            <p className='font-medium text-2xl'>Gemini <span className="text-sm text-[#ff5546]">'Dev by <span className="text-sm text-[#4b90ff]">prashant</span>'</span></p>
+            
+            <p className='font-medium text-2xl'> Gemini <span className="text-sm text-[#ff5546]">'Dev by <span className="text-sm text-[#4b90ff]">prashant</span>'</span></p>
             <img className='rounded-full w-[2em]' src={assets.user_icon} alt='profile' />
         </div>
 
         
 
-        <div className='main-container max-w-[900px] m-auto '>
+        <div className='main-container lg:max-w-[900px] m-auto '>
 
             {!showResult?
             <>
-            <div className='greet lg:text-6xl text-xl text-[#c7c4c5] font-medium py-8'>
+            <div className='greet lg:text-6xl text-4xl text-[#c7c4c5] font-medium py-10 px-4'>
                 <p><span>Hello, prashant</span></p>
                 <p>How can I help you today?</p>
             </div>
-
-            <div className='Cards py-5 pr-5 '>
-                <div className='Card'>
+                
+            <div className='Cards pt-5 pb-0 px-5 flex  overflow-auto  max-w-[22em] lg:max-w-[100%]  w-full gap-5  p-5 '>
+                <div className='Card min-w-[12em]  '>
                     <p>Suggest beautiful places to see on an upcoming road trip</p>
                     <img src={assets.compass_icon} alt='#'/>
                 </div>
-                <div className='Card'>
+                <div className='Card min-w-[12em] '>
                     <p>Help me understand American football</p>
                     <img src={assets.bulb_icon} alt='#'/>
                 </div>
-                <div className='Card'>
+                <div className='Card min-w-[12em] '>
                     <p>Evaluate and rank common camera categories</p>
                     <img src={assets.message_icon} alt='#'/>
                 </div>
-                <div className='Card'>
+                <div className='Card min-w-[12em]'>
                     <p>Give me a walkthrough of The Byzantine Empire in seven bullet points or less</p>
                     <img src={assets.code_icon} alt='#'/>
                 </div>
             </div>
+                
             </>
             :
             <div className="result py-0 px-[5%] max-h-[70vh] overflow-y-scroll">
                 <div className="result-title flex gap-4 items-center my-9 mx-0 ">
                     <img className="w-[2.3em] rounded-full" src={assets.user_icon} alt="#" />
-                    <p>{recentPrompt}</p>
+                    <p className='text-lg'>{recentPrompt}</p>
                 </div>
                 <div className="result-data flex items-start gap-5">
                     <img className='animate-pulse w-[2em]' src={assets.gemini_icon} alt="" />
@@ -73,16 +75,16 @@ function Main() {
             
         
 
-            <div className='main-bottom  absolute bottom-0 w-[100%] max-w-[900px] m-auto pt-0 pr-5 '>
-                    <div className='search-box flex items-center gap-5 justify-between  bg-[#f0f7f9] rounded-[50px] py-3 px-5  '>
-                        <input onChange={(e)=>setinput(e.target.value)}  value={input} className='w-[75%] p-2 outline-none bottom-0 bg-transparent text-[18px]' type='text' placeholder="Enter a prompt here" />
-                        <div className="flex items-center gap-4">
+            <div className='main-bottom  bottom-0 w-[100%] max-w-[900px] m-auto pt-0 pr-3 lg:pr-5 fixed '>
+                    <div className='search-box flex items-center gap-5 justify-between  bg-[#f0f7f9] rounded-[50px] py-3  px-5    '>
+                        <input onChange={(e)=>setinput(e.target.value)}  value={input} className='lg:w-[75%] w-full lg:py-2 lg:px-2 mx-3  py-5 outline-none bottom-0 bg-transparent text-[18px]' type='text' placeholder="Enter a prompt here" />
+                        <div className="flex items-center lg:gap-4 gap-6 pr-3 ">
                             <img src={assets.gallery_icon} alt="#" />
                             <img src={assets.mic_icon} alt="#" />
-                            <img onClick={()=>onSent()} src={assets.send_icon} alt="#" />
+                           {input? <img onClick={()=>onSent()} src={assets.send_icon} alt="#" />: null}
                         </div>
                     </div>
-                    <p className='text-sm font-normal text-center py-2'>Gemini may display inaccurate info, including about people, so double-check its responses. <span className='underline'> Your privacy and Gemini Apps</span></p>
+                    <p className='lg:text-sm text-[10px] font-normal text-center px-2 py-2'>Gemini may display inaccurate info, including about people, so double-check its responses. <span className='underline'> Your privacy and Gemini Apps</span></p>
             </div>
 
         </div>
